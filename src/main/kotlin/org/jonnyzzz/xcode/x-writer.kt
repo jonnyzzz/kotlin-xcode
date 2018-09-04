6262b9380b2376e3a7eb3a61d54313e4ec979d5e
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-09-04 23:42:05 +0300

    minor

--- src/main/kotlin/org/jonnyzzz/xcode/x-writer.kt
+++ src/main/kotlin/org/jonnyzzz/xcode/x-writer.kt
@@ -6,26 +6,28 @@ interface XWriter {
   fun appendLine(x: String)
 
   operator fun String.unaryPlus() = appendLine(this)
+  operator fun String.unaryMinus() = appendLine(this)
 
   infix fun String.to(x: Int) = appendLine("$this = $x;")
   infix fun String.to(x: String) = appendLine("$this = $x;")
   infix fun String.to(x: XWriter.() -> Unit) = block(prefix = "$this = ", suffix = ";", x = x)
   fun String.to(comment: String, x: XWriter.() -> Unit) = block(prefix = "$this = /* $comment */ ", suffix = ";", x = x)
+}
 
-  fun comment(x: String) = appendLine("/* $x */")
-
-  fun indent() : XWriter {
-    val that = this
-    return object : XWriter {
-      override fun appendLine() = that.appendLine()
-      override fun appendLine(x: String) = that.appendLine("    $x")
-    }
-  }
-
-  fun block(prefix: String = "", suffix : String = "", x : XWriter.() -> Unit) {
-    + "$prefix{"
-    indent().apply(x)
-    + "}$suffix"
+fun XWriter.indent() : XWriter {
+  val that = this
+  return object : XWriter {
+    override fun appendLine() = that.appendLine()
+    override fun appendLine(x: String) = that.appendLine("    $x")
   }
 }
+
+fun XWriter.block(prefix: String = "", suffix : String = "", x : XWriter.() -> Unit) {
+  + "$prefix{"
+  indent().apply(x)
+  + "}$suffix"
+}
+
+fun XWriter.comment(x: String) = appendLine("/* $x */")
+
 
